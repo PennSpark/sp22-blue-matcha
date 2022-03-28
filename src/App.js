96391c9db2ff5793087796cc75d547d19668167b
Xiaocong (Rain) Yan <50472308+xcupsilon@users.noreply.github.com>
2022-03-28 00:54:40 -0400

    sets up react app with tailwind css

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./styles/index.css"
+
+function App() {
+  return (
+    <div className='py-10 text-3xl text-center bg-green-50 shadow-lg'>
+      Hello From Matcha-makers &lt;3
+    </div>
+  );
+}
+
+export default App;
