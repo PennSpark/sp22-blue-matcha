1720c013cb4f43529c84b2e3b733fe28496a975b
Xiaocong (Rain) Yan <50472308+xcupsilon@users.noreply.github.com>
2022-04-09 18:49:09 -0400

    fixes npm yarn dependency errors

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,11 +1,26 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+
+import axios from 'axios'
 
 import pouring_tea from '../imgs/content-placeholder.gif'
 
 const Login = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+
+  const navigate = useNavigate()
+
+  const login = async () => {
+    console.log('trying to login')
+    await axios.post('/account/login', { username, password })
+      .then(() => {
+        navigate('/')
+      })
+      .catch(error => {
+        alert(`${error.response.data}`)
+      })
+  }
 
   return (
     <div className="flex justify-center mt-20">
@@ -15,13 +30,13 @@ const Login = () => {
           <h2 className="text-greentea text-2xl inline">don&apos;t have an account?</h2>
           <Link to="/signup" className="text-2xl inline"> sign up</Link>
           <div className="mb-4">
-            <input onChange={e => setEmail(e.target.value)} value={email} className="w-80 shadow border rounded-lg py-2 px-3 mt-10 text-greentea text-base leading-tight focus:outline-none focus:shadow-outline focus:border-lemon" id="email" type="text" placeholder="Email" />
+            <input onChange={e => setEmail(e.target.value)} value={email} className="w-80 shadow border rounded-lg py-2 px-3 mt-10 text-black text-base leading-tight focus:outline-none focus:shadow-outline focus:border-lemon" id="email" type="text" placeholder="Email" />
           </div>
           <div className="mb-4">
-            <input onChange={e => setPassword(e.target.value)} value={password} className="w-80 shadow border rounded-lg py-2 px-3 text-greentea text-base leading-tight focus:outline-none focus:shadow-outline focus:border-lemon" id="password" type="text" placeholder="Password" />
+            <input onChange={e => setPassword(e.target.value)} value={password} className="w-80 shadow border rounded-lg py-2 px-3 text-black text-base leading-tight focus:outline-none focus:shadow-outline focus:border-lemon" id="password" type="text" placeholder="Password" />
           </div>
-          <button type="submit" className="w-20 shadow appearance-none border rounded-lg py-2 px-3 text-orange-700 bg-orange-200 text-base leading-tight">
-            signin
+          <button onSubmit={e => login()} type="submit" className="w-20 shadow appearance-none border rounded-lg py-2 px-3 text-orange-700 bg-orange-200 text-base leading-tight">
+            login
           </button>
         </div>
         <div className="flex justify-end rounded-2xl ml-20 bg-white">
--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,4 +1,7 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router'
+
+import axios from 'axios'
 
 import pouring_tea from '../imgs/content-placeholder.gif'
 
@@ -6,6 +9,18 @@ const Login = () => {
   const [username, setUsername] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+
+  const navigate = useNavigate()
+
+  const createUser = async () => {
+    await axios.post('/account/signup', { username, password })
+      .then(() => {
+        navigate('/')
+      })
+      .catch(error => {
+        alert(`${error.response.data}`)
+      })
+  }
 
   return (
     <div className="flex justify-center mt-20">
@@ -19,12 +34,12 @@ const Login = () => {
             <input onChange={e => setUsername(e.target.value)} value={username} className="w-80 shadow border rounded-lg py-2 px-3 mt-10 text-greentea text-base leading-tight focus:outline-none focus:shadow-outline focus:border-lemon" id="username" type="text" placeholder="Username" />
           </div>
           <div className="mb-4">
-            <input onChange={e => setEmail(e.target.value)} value={email} className="w-80 shadow border rounded-lg py-2 px-3 text-greentea text-base leading-tight focus:outline-none focus:shadow-outline focus:border-lemon" id="email" type="text" placeholder="Email" />
+            <input onChange={e => setEmail(e.target.value)} value={email} className="w-80 shadow border rounded-lg py-2 px-3 text-black text-base leading-tight focus:outline-none focus:shadow-outline focus:border-lemon" id="email" type="text" placeholder="Email" />
           </div>
           <div className="mb-4">
-            <input onChange={e => setPassword(e.target.value)} value={password} className="w-80 shadow border rounded-lg py-2 px-3 text-greentea text-base leading-tight focus:outline-none focus:shadow-outline focus:border-lemon" id="password" type="text" placeholder="Password" />
+            <input onChange={e => setPassword(e.target.value)} value={password} className="w-80 shadow border rounded-lg py-2 px-3 text-black text-base leading-tight focus:outline-none focus:shadow-outline focus:border-lemon" id="password" type="text" placeholder="Password" />
           </div>
-          <button type="submit" className="w-20 shadow appearance-none border rounded-lg py-2 px-3 text-orange-700 bg-orange-200 text-base leading-tight">
+          <button onSubmit={e => createUser()} type="submit" className="w-20 shadow appearance-none border rounded-lg py-2 px-3 text-orange-700 bg-orange-200 text-base leading-tight">
             signup
           </button>
         </div>
