dc42908f5cd8833c97fa56dd9af343bdbd453614
Xiaocong (Rain) Yan <50472308+xcupsilon@users.noreply.github.com>
2022-04-09 23:22:49 -0400

    finishes styling login, signup; redesigns navbar and home

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -24,31 +24,27 @@ const Login = () => {
   }
 
   return (
-    <>
-      <NavBar />
-      <div className="flex justify-center mt-20">
-        <div className="flex bg-light_matcha w-3/4 h-4/6 p-20 rounded-3xl shadow-lg">
-          <div className="justify-start">
-            <h1 className="text-dark_matcha font-semibold text-7xl font-mono">welcome!</h1>
-            <h2 className="text-greentea text-2xl inline">don&apos;t have an account?</h2>
-            <Link to="/signup" className="text-2xl inline"> sign up</Link>
-            <div className="mb-4">
-              <input onChange={e => setEmail(e.target.value)} value={email} className="w-80 shadow border rounded-lg py-2 px-3 mt-10 text-black text-base leading-tight focus:outline-none focus:shadow-outline focus:border-lemon" id="email" type="text" placeholder="Email" />
-            </div>
-            <div className="mb-4">
-              <input onChange={e => setPassword(e.target.value)} value={password} className="w-80 shadow border rounded-lg py-2 px-3 text-black text-base leading-tight focus:outline-none focus:shadow-outline focus:border-lemon" id="password" type="text" placeholder="Password" />
-            </div>
-            <button onClick={e => login()} type='submit' className="w-20 shadow appearance-none border rounded-lg py-2 px-3 text-orange-700 bg-orange-200 text-base leading-tight">
-              login
-            </button>
+    <div className="flex justify-center mt-20">
+      <div className="grid grid-cols-5 bg-light_matcha w-3/4 h-4/6 p-20 pl-10 rounded-3xl shadow-lg">
+        <div className="col-span-2 flex flex-col justify-center items-center">
+          <h1 className="text-dark_matcha font-semibold text-7xl font-mono mb-2 mt-8">Welcome!</h1>
+          <h2 className="text-greentea text-2xl">
+            don&apos;t have an account?
+            <Link to="/signup" className="text-2xl text-black inline"> sign up</Link>
+          </h2>
+          <div className="mb-4">
+            <input onChange={e => setEmail(e.target.value)} value={email} className="w-80 shadow border rounded-xl py-4 px-3 mt-16 text-center text-black text-lg leading-tight focus:outline-none focus:shadow-outline focus:border-lemon" id="email" type="text" placeholder="Email" />
           </div>
-          <div className="flex justify-end rounded-2xl ml-20 bg-white">
-            <img src={pouring_tea} alt="" className="rounded-lg shadow-md hover:shadow-xl py-10 px-12" />
+          <div className="mb-4">
+            <input onChange={e => setPassword(e.target.value)} value={password} className="w-80 shadow border rounded-xl py-4 px-3 mt-2 text-center text-black text-lg leading-tight focus:outline-none focus:shadow-outline focus:border-lemon" id="password" type="text" placeholder="Password" />
           </div>
+          <button onClick={e => login()} type='submit' className="w-60 shadow appearance-none border rounded-xl py-4 px-3 mt-2 mb-8 text-orange-700 bg-orange-200 text-lg leading-tight">
+            login
+          </button>
         </div>
+        <img src={pouring_tea} alt="" className="object-cover col-span-3 h-full w-5/6 rounded-2xl ml-20 bg-white shadow-md hover:shadow-xl py-10 px-12" />
       </div>
-    </>
-
+    </div>
   )
 }
 
--- frontend/src/components/Survey.jsx
+++ frontend/src/components/Survey.jsx
@@ -3,15 +3,17 @@ import NavBar from './NavBar'
 
 import axios from 'axios'
 
+// import Triangle from 'react-triangle'
+
 const Survey = () => {
   const [questions, setQuestions] = useState([])
 
   const Left = () => {
-
+    return <Triangle direction="left" x={0} y={0} size={50} />
   }
 
   const Right = () => {
-    
+    return <Triangle direction="right" x={0} y={0} size={50} />
   }
 
   return (
@@ -19,11 +21,15 @@ const Survey = () => {
       <NavBar />
       <div className="w-screen h-screen py-10 bg-greentea font-mono text-dark_matcha">
         <div className="flex justify-center">
-          <div className="bg-light_matcha w-3/4 h-4/6 p-14 rounded-3xl shadow-lg">
-            <h1 className="ml-5 mb-10 text-2xl">Question blah blah blah?</h1>
-            <div className="flex flex-col">
-              <button type="button" className="bg-white rounded-xl py-5 px-12 text-left focus:bg-lemon">&gt; a for effort</button>
+          <div className="flex flex-row bg-light_matcha w-3/4 h-4/6 p-14 rounded-3xl shadow-lg">
+            {/* <Left /> */}
+            <div>
+              <h1 className="ml-5 mb-10 text-2xl">Question blah blah blah?</h1>
+              <div className="flex flex-col">
+                <button type="button" className="bg-white rounded-xl py-5 px-12 text-left focus:bg-lemon">&gt; a for effort</button>
+              </div>
             </div>
+            {/* <Right /> */}
           </div>
         </div>
       </div>
