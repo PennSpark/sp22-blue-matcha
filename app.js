97a690c48b203ba05bd14158be59a9b1254b6132
Alyssa Nie <niealyssa@gmail.com>
2022-04-27 16:31:01 -0400

    change path

--- app.js
+++ app.js
@@ -30,9 +30,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../frontend/public')));
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
+  app.use(express.static(path.join(__dirname, 'frontend', 'build')));
   app.get("*", function (request, response) {
-    response.sendFile(path.resolve(__dirname, "./frontend/build", "index.html"));
+    response.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   });
 }
 
