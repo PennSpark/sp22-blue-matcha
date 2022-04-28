2a0216cd3828cdfde6127fed8264f0d5a9592b84
Alyssa Nie <niealyssa@gmail.com>
2022-04-28 11:03:27 -0400

    pain

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(express.static(path.join(__dirname, '/frontend/public')));
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
   app.use(express.static(path.resolve(__dirname, 'frontend', 'build'), {extensions: ["js"]}));
   app.get("*", function (request, response) {
-    response.sendFile(path.join(__dirname, '/frontend/build/index.html'));
+    response.sendFile(path.join(__dirname, '/frontend/build'));
   });
 }
 app.use(session({
