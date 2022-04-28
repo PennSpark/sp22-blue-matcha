c123eacc408ea8a512bbcf0e16b90083fdfb11d6
Alyssa Nie <niealyssa@gmail.com>
2022-04-28 11:46:21 -0400

    confusion

--- app.js
+++ app.js
@@ -26,12 +26,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '/frontend/public')));
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
   app.use(express.static(path.resolve(__dirname, 'frontend', 'build'), {extensions: ["js"]}));
   app.get("*", function (request, response) {
     response.sendFile(path.join(__dirname, '/frontend/build/index.html'));
   });
+} else {
+  app.use(express.static(path.join(__dirname, '/frontend/public')));
 }
 app.use(session({
   secret: 'keyboard cat',
