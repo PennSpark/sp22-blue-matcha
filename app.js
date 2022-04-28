9c32819451a8bf80be94583b52e51585e2e241ad
Alyssa Nie <niealyssa@gmail.com>
2022-04-28 12:01:12 -0400

    ok jk

--- app.js
+++ app.js
@@ -26,14 +26,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
-  app.use(express.static(path.resolve(__dirname, 'frontend', 'build'), {extensions: ["js"]}));
-  app.get("*", function (request, response) {
-    response.sendFile(path.join(__dirname, '/frontend/build/index.html'));
-  });
-} else {
-  //app.use(express.static(path.join(__dirname, '/frontend/public')));
-}
+
+app.use(express.static(path.resolve(__dirname, 'frontend', 'build'), {extensions: ["js"]}));
+app.get("*", function (request, response) {
+  response.sendFile(path.join(__dirname, '/frontend/build/index.html'));
+
 app.use(session({
   secret: 'keyboard cat',
   resave: false,
