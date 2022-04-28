8632edbac3f04021561837c6bdebc59fd961234f
Xiaocong (Rain) Yan <50472308+xcupsilon@users.noreply.github.com>
2022-04-28 15:42:22 -0400

    adds flippable cards for gallery

--- frontend/src/components/Gallery.jsx
+++ frontend/src/components/Gallery.jsx
@@ -36,11 +36,13 @@ const Gallery = () => {
   //   return () => clearInterval(intervalID)
   // }, [])
   
-  const GalleryModel = ({allCards}) => allCards.map(card => {
-      const date = new Date(card.date)
-      let formatted_date = date.toDateString()
-      return (<GalleryCard picture={card.photo ? card.photo.image_url : DEFAULT} pairName={card.people} date={card.date ? formatted_date : ''} facts={card.facts} />)
-    })
+  const GalleryModel = ({ allCards }) => allCards.map(card => {
+    const date = new Date(card.date)
+    let formatted_date = date.toDateString()
+    return (
+      <GalleryCard picture={card.photo ? card.photo.image_url : DEFAULT} pairName={card.people} date={card.date ? formatted_date : ''} facts={card.facts} />
+    )
+  })
 
   const CardModal = () => {
     if (modalVisible) {
@@ -51,30 +53,26 @@ const Gallery = () => {
 
   // TODO: uncomment dynamic generation, delete hard coded card
   return (
-    <div className="bg-greentea w-screen h-screen">
-      <div className="flex justify-center font-semibold pt-10 capitalize">
-        <h1 className="inline text-5xl text-dark_matcha">
-          coffee chat gallery
-        </h1>
-        <button onClick={e => setModalVisible(true)} button="button" className="relative top-2 inline w-10 h-10 ml-5 rounded-xl text-3xl bg-dark_matcha text-matcha">
-          <div className="relative bottom-0.5">
-            +
-          </div>
-        </button>
-        <CardModal />
-      </div>
+    <>
+      <NavBar />
+      <div className="bg-greentea w-screen h-screen">
+        <div className="flex justify-center font-semibold pt-10 capitalize">
+          <h1 className="inline text-5xl mt-10 text-dark_matcha">
+            coffee chat gallery
+          </h1>
+          <button onClick={e => setModalVisible(true)} button="button" className="relative top-12 inline w-10 h-10 ml-5 rounded-xl text-3xl bg-dark_matcha text-matcha">
+            <div className="relative bottom-0.5">
+              +
+            </div>
+          </button>
+          <CardModal />
+        </div>
 
-      <div className="grid grid-cols-3 gap-10 p-10 bg-greentea">
-        {receivedCards && <GalleryModel allCards={cards}/>}
-        <GalleryCard picture={DEFAULT} pairName={'Andrew and Ethan'} date={'Saturday'} facts={['Ethan is cool', 'Andrew is uwu', 'Food was mid', 'Temp is nice']} />
-        <GalleryCard picture={'https://ca.slack-edge.com/T02BG31SB7H-U02G075615F-93330ae64fe8-512'} pairName={'Andrew and Ethan'} date={'Saturday'} facts={['Ethan is cool', 'Andrew is uwu', 'Food was mid', 'Temp is nice']} />
-        <GalleryCard picture={'https://ca.slack-edge.com/T02BG31SB7H-U02G075615F-93330ae64fe8-512'} pairName={'Andrew and Ethan'} date={'Saturday'} facts={['Ethan is cool', 'Andrew is uwu', 'Food was mid', 'Temp is nice']} />
-        <GalleryCard picture={'https://ca.slack-edge.com/T02BG31SB7H-U02G075615F-93330ae64fe8-512'} pairName={'Andrew and Ethan'} date={'Saturday'} facts={['Ethan is cool', 'Andrew is uwu', 'Food was mid', 'Temp is nice']} />
-        <GalleryCard picture={'https://ca.slack-edge.com/T02BG31SB7H-U02G075615F-93330ae64fe8-512'} pairName={'Andrew and Ethan'} date={'Saturday'} facts={['Ethan is cool', 'Andrew is uwu', 'Food was mid', 'Temp is nice']} />
-        <GalleryCard picture={'https://ca.slack-edge.com/T02BG31SB7H-U02G075615F-93330ae64fe8-512'} pairName={'Andrew and Ethan'} date={'Saturday'} facts={['Ethan is cool', 'Andrew is uwu', 'Food was mid', 'Temp is nice']} />
-        {/* {cards && cards.reverse.map(card => generateCardBlock(card))} */}
+        <div className="grid grid-cols-3 justify-items-center justify-start gap-10 p-10 bg-greentea">
+          {receivedCards && <GalleryModel allCards={cards}/>}
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
--- frontend/src/components/GalleryCard.jsx
+++ frontend/src/components/GalleryCard.jsx
@@ -1,39 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ReactCardFlip from 'react-card-flip'
 
-const GalleryCard = ({ picture, pairName, date, facts }) => (
-  <div className="flex flex-col justify-center mt-10 p-10 px-16 pb-12 bg-light_matcha shadow-xl rounded-3xl text-2xl lowercase">
-    <img src={picture} alt="" className="object-cover self-center w-full h-full mb-2 rounded-2xl shadow-md hover:shadow-lg" />
-    <div className='mt-8'>
-      <h2 className="text-dark_matcha inline font-bold">
-        chat pair:&nbsp;
-      </h2>
-      <div className="text-dark_greentea inline">
-        {pairName}
-      </div>
-    </div>
-    <div className='mt-5'>
-      <h2 className="text-dark_matcha inline font-bold">
-        date:&nbsp;
-      </h2>
-      <div className="text-dark_greentea inline">
-        {date}
-      </div>
-    </div>
-    <div className='mt-5'>
-      <h2 className="text-dark_matcha inline font-bold">
-        interesting fax:&nbsp;
-      </h2>
-      <div className="flex flex-col text-dark_greentea">
-        {facts.map((fact, index) => {
-          return (
-            <div>
-              &gt; {fact}
-            </div>
-          )
-        })}
-      </div>
-    </div>
-  </div>
-)
+import GalleryCardFront from './GalleryCardFront'
+import GalleryCardBack from './GalleryCardBack'
+
+const GalleryCard = ({ picture, pairName, date, facts }) => {
+  const [isFlipped, setIsFlipped] = useState(false)
+  
+  const handleClick = e => {
+    e.preventDefault()
+    setIsFlipped(!isFlipped)
+  }  
+
+  return (
+    <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal" containerClassName="w-full">
+      <button onClick={e => handleClick(e)} type='button' className='w-full'>
+        <GalleryCardFront picture={picture} date={date} />
+      </button>
+        
+      <button onClick={e => handleClick(e)} type='button' className='w-full'>
+        <GalleryCardBack pairName={pairName} facts={facts} />
+      </button>
+    </ReactCardFlip>
+  )
+}
 
 export default GalleryCard
