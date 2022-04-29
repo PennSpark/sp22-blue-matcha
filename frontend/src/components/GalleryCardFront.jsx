3c6aac3e50691f7694b8ce80d67b0ea814e1990f
dmed31 <dhatrim@seas.upenn.edu>
2022-04-29 02:16:25 -0400

    capitalized dates

--- frontend/src/components/GalleryCardFront.jsx
+++ frontend/src/components/GalleryCardFront.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const GalleryCardFront = ({ picture, date }) => (
   <div className="flex flex-col justify-center mt-10 pt-8 px-8 pb-10 h-[650px] bg-light_greentea shadow-lg drop-shadow-svg_lighter hover:shadow-darker rounded-xl text-2xl lowercase">
     <img src={picture} alt="" className="object-cover self-center w-full h-[500px] shadow-md border-4 border-dark_greentea" />
-    <div className="mt-10 text-dark_matcha drop-shadow-sm font-mono italic">
+    <div className="mt-10 text-dark_matcha drop-shadow-sm font-mono italic capitalize">
       {date}
     </div>
   </div>
